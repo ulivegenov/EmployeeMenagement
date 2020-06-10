0e619d1fa9eb8cfd0d8e15074acef20f74907af4
ulivegenov <uli_vegenov@abv.bg>
2020-06-10 11:09:02 +0300

    1/ Add EmployeeManagement.Api/Controllers/EmployeesController.cs. 2/ Implement GetEmployees HttpGet method in EmployeesController.

---
+++ src/EmployeeManagementSystem/EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeeManagement.Api.Controllers
+{
+    using EmployeeManagement.Api.Repositories.Contracts;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await this.employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database!");
+            }
+        }
+    }
+}
